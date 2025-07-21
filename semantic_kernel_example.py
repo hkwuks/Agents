5695ced4c24ef8b3a727321315e8bc11ba644c74
hkwuks <hkwuks@qq.com>
2025-07-22 00:34:39 +0800

    Update semantic_kernel_example.py

--- semantic_kernel_example.py
+++ semantic_kernel_example.py
@@ -3,6 +3,7 @@ from typing import Annotated
 
 from semantic_kernel.connectors.ai import FunctionChoiceBehavior
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion, AzureChatPromptExecutionSettings
+from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
 from semantic_kernel.contents import ChatHistory
 from semantic_kernel.functions import kernel_function
 from semantic_kernel.kernel import Kernel
@@ -18,11 +19,12 @@ class BookTravelPlugin:
                           location: Annotated[str, "The location to travel to"]) -> str:
         return f'Travel was booked to {location} on {date}'
 
+
 # Create a kernel
-kernel=Kernel()
+kernel = Kernel()
 
 # Add the sample plugin to the kernel object
-kernel.add_plugin(BookTravelPlugin(),plugin_name='book_travel')
+kernel.add_plugin(BookTravelPlugin(), plugin_name='book_travel')
 
 # Define the Azure OpenAI AI Connector
 chat_service = AzureChatCompletion(
@@ -30,3 +32,49 @@ chat_service = AzureChatCompletion(
     api_key='YOUR_API_KEY',
     endpoint='https://<your-resource>.azure.openai.com/'
 )
+
+# Define the request settings to configure the model with auto-function calling
+request_settings = PromptExecutionSettings(function_choice_behavior=FunctionChoiceBehavior.Auto())
+
+
+async def main():
+    # Make the request to the model for the given chat history and request
+    # The Kernel contains the sample that model will request to invoke
+    response = await chat_service.get_chat_message_content(
+        chat_history=chat_history, settings=request_settings, kernel=kernel
+    )
+    assert response is not None
+
+    '''
+    Note: In the auto function calling process, the model determines it can invoke the 'BookTravelPlugin' using the
+    'book_flight' function, supplying the necessary arguments.
+    
+    For example:
+    
+    "tool_calls":[
+        {
+            "id":"call_abc123",
+            "type":"function",
+            "function":{
+                "name":"BookTravelPlugin-book_flight",
+                "argument":"{'location': 'New York', 'date': '2025-01-01'}"
+            }
+        }
+    ]
+    
+    Since the location and date arguments are required (as defined by the kernel function), if the model lacks either,
+    it will prompt the user to provide them. For instance:
+    
+    User: Book me a flight to New York.
+    Model: Sure, I'd love to help you book a flight. Could you please specify the date?
+    User: I want to travel on January 1, 2025.
+    Model: Your flight to New York on January 1, 2025, has been successfully booked. Safe travels!
+    '''
+
+    print(response)
+
+    chat_history.add_assistant_message(response.content)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
